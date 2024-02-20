80593456ae957b1c8ff30070f7f528a8940d10ff
SKALEZ-A <holumacbeth@gmail.com>
2024-02-20 16:11:14 +0100

    pay

--- app/pay/page.jsx
+++ app/pay/page.jsx
@@ -1,24 +1,29 @@
 import React from "react";
+import Image from "next/image";
+import bnw from "@/public/images/black_n_white.jpeg";
 
 const page = () => {
   return (
     <div>
       <div className="">
         <div className="cont">
-          <span></span>
+          <span className="">
+            {/* <Image src={bnw} alt="user icon" className="w-full" /> */}
+          </span>
+
           <p>0xbaFC....9573</p>
         </div>
-        <div className="w-[auto] justify-center sec_div">
+        <div className="w-[auto] justify-center sec_div mx-auto sm:max-w-xl md:max-w-5xl lg:max-w-5xl xl:max-w-5xl md:container px-10">
           <div className=" text-sm flex flex-col sub">
-            <label className="text-[#d5b380] ">
+            <label className="text-gray-700 ">
               Zakat Amount <span>(2.5% of total asset)</span>{" "}
             </label>
-            <input id="file" class=" p-5 rounded-lg m-5  border" />
+            <input id="file" class=" p-5 rounded-lg my-5  border" />
 
-            <label className="text-[#d5b380] ">Message </label>
+            <label className="text-gray-700 ">Message </label>
             <input
               id="file"
-              class=" p-5 rounded-lg m-5  border"
+              class=" p-5 rounded-lg my-5  border"
               placeholder="(optional)"
             />
 
